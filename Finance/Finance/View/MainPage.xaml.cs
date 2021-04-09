b0143f57a902fd57ef7cf366c169b292b5904c3e
Arpit Shah <Arpit.Shah@einfochips.com>
2021-04-09 11:54:15 +0530

    Add project files.

---
+++ Finance/Finance/View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Finance.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //for iphone 10 onwards for notch screen
+            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+        }
+    }
+}
