53a36eb853f588f8cbab2170d754ace337a3889e
23Shah <23.arpit.it@gmail.com>
2021-04-13 16:50:14 +0530

    Changes done for Track Event, Analytics and Error Tracking

--- Finance/Finance/App.xaml.cs
+++ Finance/Finance/App.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Finance.View;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -14,8 +17,20 @@ namespace Finance
             MainPage = new NavigationPage(new MainPage());
         }
 
-        protected override void OnStart()
+        protected override async void OnStart()
         {
+            string androidAppSecret = "6ac0e0f2-579c-41c6-9ec8-7b4474ebcaab";
+            string iOSAppSecret = "0b687c29-7a3e-429a-aec4-92ecd96edd57";
+
+            AppCenter.Start($"android={androidAppSecret};ios={iOSAppSecret};", typeof(Crashes),typeof(Analytics));
+
+            //evaluate application crash
+            bool didAppCrashed = await Crashes.HasCrashedInLastSessionAsync();
+            if(didAppCrashed)
+            {
+                var crashReport = await Crashes.GetLastSessionCrashReportAsync();
+            }
+
         }
 
         protected override void OnSleep()
--- Finance/Finance/View/PostPage.xaml.cs
+++ Finance/Finance/View/PostPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using Finance.Model;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace Finance.View
@@ -16,9 +18,41 @@ namespace Finance.View
         public PostPage(Item item)
         {
             InitializeComponent();
-            Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+            try
+            {
+                //throw (new Exception("Unable to load blog")); //for generating Error which will be available in appcenter
+                Xamarin.Forms.PlatformConfiguration.iOSSpecific.Page.SetUseSafeArea(this, true);
+                webView.Source = item.ItemLink;
+                var properties = new Dictionary<string, string>
+                {
+                    { "Blog_post ",$"{item.Title}"}
+                };
+                TrackEvent(properties);
+            }
+            catch (Exception ex)
+            {
+                var properties = new Dictionary<string, string>
+                {
+                    { "Blog_post ",$"{item.Title}"}
+                };
 
-            webView.Source = item.ItemLink;
+                TrackError(ex, properties);
+            }
+           
+        }
+
+        private async void TrackEvent(Dictionary<string, string> properties)
+        {
+            if(await Analytics.IsEnabledAsync())
+                Analytics.TrackEvent("Blog Post Opened", properties);
+
+        }
+
+        private async void TrackError(Exception ex, Dictionary<string, string> properties)
+        {
+            if (await Crashes.IsEnabledAsync())
+                Crashes.TrackError(ex, properties);
+
         }
     }
 }
